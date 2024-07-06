4a43ad65b81524411e486de0810e9668ac0714df
Ziyad Khan <ziyadkhan2333@gmail.com>
2024-07-06 21:38:53 +0500

    Environement variable functionality done

--- Database/ORM.h
+++ Database/ORM.h
@@ -9,36 +9,36 @@
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
 #include <vector>
-
+#include "../Env/env.h"
 using namespace std;
 using namespace sql;
 
-class ORM {
+class ORM : public Environment {
     protected:
         Driver *driver;
         Connection *connection;
         Statement *statement;
         ResultSet *resultset;
-        vector <string> connectionProps = {"tcp://127.0.0.1:3306" , "ziyad" , "root" , "chatapp"};
-        Connection* createConnection( vector<string> connectionProperties); 
+        // vector <string> connectionProps = {"tcp://127.0.0.1:3306" , "ziyad" , "root" , "chatapp"};
+        Connection* createConnection( ); 
 
         void createDatabaseIfNotExists(string database);
         
         bool checkIfDatabaseExists(string dbName);
         bool checkIfTableExists(string tableName);
 
-        bool presetOrCustomSettings();  
+        // bool presetOrCustomSettings();  
 
 
     public:
         ORM();
 
-        vector<string> getPropertiesFromUser();
+        // vector<string> getPropertiesFromUser();
         ResultSet* raw(string query);
         void insert(string tablename , string columns , string values);
         string parseSingleString(ResultSet *str);
         int parseSingleInt(ResultSet *str);
-        Connection* connect();
+        // Connection* connect();
 
 
 };
